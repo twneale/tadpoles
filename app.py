07dc84478f13934ebc99c875f565079ac5ef9405
twneale <twneale@gmail.com>
2015-12-30 12:59:23 -0500

    Big cleanup done

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ import pickle
 import logging
 import logging.config
 
-from random import random
+from random import randrange
 from getpass import getpass
+from os.path import abspath, dirname, join, isfile, isdir
 
 import requests
 import lxml.html
@@ -49,110 +50,193 @@ LOGGING_CONFIG = {
     },
 }
 logging.config.dictConfig(LOGGING_CONFIG)
-logger = logging.getLogger('app')
-info = logger.info
-debug = logger.debug
-warning = logger.warning
-critical = logger.critical
 
 
 # -----------------------------------------------------------------------------
 # The scraper code.
 # -----------------------------------------------------------------------------
-SLEEP = 1
+class Client:
 
-class Headless:
+    COOKIE_FILE = "cookies.pkl"
+    ROOT_URL = "http://tadpoles.com/"
+    HOME_URL = "https://www.tadpoles.com/parents"
+    MIN_SLEEP = 1
+    MAX_SLEEP = 3
+
     def __init__(self):
-        # pass
-        self.vdisplay = Xvfb()
+        self.init_logging()
+
+    def init_logging(self):
+        logger = logging.getLogger('app')
+        self.info = logger.info
+        self.debug = logger.debug
+        self.warning = logger.warning
+        self.critical = logger.critical
+        self.exception = logger.exception
 
     def __enter__(self):
-        logger.info("Starting xvfb display")
+        self.info("Starting xvfb display")
+        self.vdisplay = Xvfb()
         self.vdisplay.start()
-        logger.info("Starting browser")
-       # profile = webdriver.FirefoxProfile()
-#        profile.set_preference("javascript.enabled", False);
+        self.info("Starting browser")
         self.br = self.browser = webdriver.Firefox()
         self.br.implicitly_wait(10)
         return self
 
     def __exit__(self, *args):
-        logger.info("Shutting down browser")
+        self.info("Shutting down browser")
         self.browser.quit()
-        logger.info("Shutting down xfvb display")
+        self.info("Shutting down xfvb display")
         self.vdisplay.stop()
 
+    def sleep(self, minsleep=None, maxsleep=None):
+        _min = minsleep or self.MIN_SLEEP
+        _max = maxsleep or self.MAX_SLEEP
+        duration = randrange(_min * 100, _max * 100) / 100.0
+        self.debug('Sleeping %r' % duration)
+        time.sleep(duration)
 
-def download(hd):
-    br = hd.browser
+    def navigate_url(self, url):
+        self.info("Navigating to %r", url)
+        self.br.get(url)
 
-    # Hit the homepage and start a session.
-    url = "http://tadpoles.com/"
-    info("Fetching %r", url)
-    br.get(url)
+    def load_cookies(self):
+        self.info("Loading cookies.")
+        with open(self.COOKIE_FILE, "rb") as f:
+            self.cookies = pickle.load(f)
 
-    # Add cookies.
-    try:
-        info("Loading cookies")
-        with open("cookies.pkl", "rb") as f:
-            cookies = pickle.load(f)
-    except FileNotFoundError:
-        br.find_element_by_id("login-button").click()
-        br.find_element_by_class_name("tp-block-half").click()
-        br.find_element_by_class_name("other-login-button").click()
-        window = set(br.window_handles) - set([br.current_window_handle])
-        br.switch_to.window(window.pop())
+    def dump_cookies(self):
+        self.info("Dumping cookies.")
+        with open(self.COOKIE_FILE,"wb") as f:
+            pickle.dump(self.br.get_cookies(), f)
 
-        email = br.find_element_by_id("Email")
+    def add_cookies_to_browser(self):
+        self.info("Adding the cookies to the browser.")
+        for cookie in self.cookies:
+            if self.br.current_url.strip('/').endswith(cookie['domain']):
+                self.br.add_cookie(cookie)
+
+    def requestify_cookies(self):
+        # Cookies in the form reqeusts expects.
+        self.info("Transforming the cookies for requests lib.")
+        self.req_cookies = {}
+        for s_cookie in self.cookies:
+            self.req_cookies[s_cookie["name"]] = s_cookie["value"]
+
+    def switch_windows(self):
+        '''Switch to the other window.'''
+        self.info("Switching windows.")
+        all_windows = set(self.br.window_handles)
+        current_window = set([self.br.current_window_handle])
+        other_window = (all_windows - current_window).pop()
+        br.switch_to.window(other_window)
+
+    def do_login(self):
+        # Navigate to login page.
+        self.info("Navigating to login page.")
+        self.br.find_element_by_id("login-button").click()
+        self.br.find_element_by_class_name("tp-block-half").click()
+        self.br.find_element_by_class_name("other-login-button").click()
+
+        # Focus on the google auth popup.
+        self.switch_windows()
+
+        # Enter email.
+        email = self.br.find_element_by_id("Email")
         email.send_keys(input("Enter email: "))
         email.submit()
 
-        passwd = br.find_element_by_id("Passwd")
+        # Enter password.
+        passwd = self.br.find_element_by_id("Passwd")
         passwd.send_keys(getpass("Enter password:"))
         passwd.submit()
 
-        pin = br.find_element_by_id("idvPreregisteredPhonePin")
+        # Enter 2FA pin.
+        pin = self.br.find_element_by_id("idvPreregisteredPhonePin")
         pin.send_keys(getpass("Enter google verification code: "))
         pin.submit()
 
-        # Approve.
-        time.sleep(2)
-        br.find_element_by_id("submit_approve_access").click()
+        # Click "approve".
+        self.info("Sleeping 2 seconds.")
+        self.sleep(minsleep=2)
+        self.info("Clicking 'approve' button.")
+        self.br.find_element_by_id("submit_approve_access").click()
 
         # Switch back to tadpoles.
-        window = set(br.window_handles) - set([br.current_window_handle])
-        br.switch_to.window(window.pop())
+        self.switch_windows()
 
-        # Now load tadpoles cookies.
-        with open("cookies.pkl","wb") as f:
-            pickle.dump(br.get_cookies(), f)
+    def iter_monthyear(self):
+        '''Yields pairs of xpaths for each year/month tile on the
+        right hand side of the user's home page.
+        '''
+        month_xpath_tmpl = '//*[@id="app"]/div[4]/div[1]/ul/li[%d]/div/div/div/div/span[%d]'
+        month_index = 1
+        while True:
+            month_xpath = month_xpath_tmpl % (month_index, 1)
+            year_xpath = month_xpath_tmpl % (month_index, 2)
 
-    else:
-        for cookie in cookies:
-            if br.current_url.strip('/').endswith(cookie['domain']):
-                br.add_cookie(cookie)
-        br.get("https://www.tadpoles.com/parents")
+            # Go home if not there already.
+            if self.br.current_url != self.HOME_URL:
+                self.navigate_url(self.HOME_URL)
+            try:
+                # Find the next month and year elements.
+                month = self.br.find_element_by_xpath(month_xpath)
+                year = self.br.find_element_by_xpath(year_xpath)
+            except NoSuchElementException:
+                # We reached the end of months on the profile page.
+                self.warning("No months left to scrape. Stopping.")
+                sys.exit(0)
 
-    # Cookies in the form reqeusts expects.
-    req_cookies = {}
-    for s_cookie in cookies:
-        req_cookies[s_cookie["name"]] = s_cookie["value"]
+            self.month = month
+            self.year = year
+            yield month, year
 
-    def get_urls(br):
-        urls = []
-        re_url = re.compile('\("([^"]+)')
-        for div in br.find_elements_by_xpath("//li/div"):
-            url = re_url.search(div.get_attribute("style"))
-            if not url:
-                continue
-            url = url.group(1)
-            url = url.replace('thumbnail=true', '').replace('&thumbnail=true', '')
-            url = 'https://www.tadpoles.com' + url
-            urls.append(url)
-        return urls
+            month_index += 1
 
-    def save_image(url, filename):
-        resp = requests.get(url, cookies=req_cookies, stream=True)
+    def iter_urls(self):
+        '''Find all the image urls on the current page.
+        '''
+        # For each month on the dashboard...
+        for month, year in self.iter_monthyear():
+            # Navigate to the next month.
+            month.click()
+            self.warning("Getting urls for month: %r" % month.text)
+            self.sleep(minsleep=2)
+            re_url = re.compile('\("([^"]+)')
+            for div in self.br.find_elements_by_xpath("//li/div"):
+                url = re_url.search(div.get_attribute("style"))
+                if not url:
+                    continue
+                url = url.group(1)
+                url = url.replace('thumbnail=true', '')
+                url = url.replace('&thumbnail=true', '')
+                url = 'https://www.tadpoles.com' + url
+                yield url
+
+    def save_image(self, url):
+        '''Save an image locally using requests.
+        '''
+
+        # Make the local filename.
+        _, key = url.split("key=")
+        filename_parts = ['img', self.year.text, self.month.text, '%s.jpg']
+        filename = abspath(join(*filename_parts) % key)
+
+        # Only download if the file doesn't already exist.
+        if isfile(filename):
+            self.debug("Already downloaded: %s" % filename)
+            return
+        else:
+            self.info("Saving: %s" % filename)
+            self.sleep()
+
+        # Make sure the parent dir exists.
+        dr = dirname(filename)
+        if not isdir(dr):
+            os.makedirs(dr)
+
+        # Download it with requests.
+        resp = requests.get(url, cookies=self.req_cookies, stream=True)
         if resp.status_code == 200:
             with open(filename, 'wb') as f:
                 for chunk in resp.iter_content(1024):
@@ -160,60 +244,38 @@ def download(hd):
         else:
             raise Exception('Got error %d' % resp.status_code)
 
-    # For each month on the dashboard...
-    month_xpath_tmpl = '//*[@id="app"]/div[4]/div[1]/ul/li[%d]/div/div/div/div/span[%d]'
-    month_index = 1
-    home_url = 'https://www.tadpoles.com/parents'
-    MAX_SLEEP = 3
-    while True:
-        month_xpath = month_xpath_tmpl % (month_index, 1)
-        year_xpath = month_xpath_tmpl % (month_index, 2)
+    def download_images(self):
+        '''Login to tadpoles.com and download all user's images.
+        '''
+        self.navigate_url(self.ROOT_URL)
+
         try:
-            # Go home if not there already.
-            if br.current_url != home_url:
-                br.get(home_url)
-            # Click the next month.
-            month = br.find_element_by_xpath(month_xpath)
-            month_name = month.text
-            year_name = br.find_element_by_xpath(year_xpath).text
-        except NoSuchElementException:
-            warning("No months left to scrape. Stopping.")
-            sys.exit(0)
+            self.load_cookies()
+        except FileNotFoundError:
+            self.do_login()
+            self.dump_cookies()
         else:
-            warning("Starting month #%d" % month_index)
-        month.click()
+            self.add_cookies_to_browser()
+            self.navigate_url(self.HOME_URL)
 
-        # ...get the urls...
-        time.sleep(2)
-        urls = get_urls(br)
-        # ...and fetch them one by one.
-        while urls:
-            url = urls.pop()
-            _, key = url.split("key=")
-            br.get(url)
+        # Get the cookies ready for requests lib.
+        self.requestify_cookies()
 
-            filename_parts = ['img', year_name, month_name, '%s.jpg' % key]
-            filename = os.path.abspath(os.path.join(*filename_parts))
-            if not os.path.isfile(filename):
-                info("Saving: %s" % filename)
-                dirname = os.path.dirname(filename)
-                if not os.path.isdir(dirname):
-                    os.makedirs(dirname)
-                save_image(url, filename)
-                # br.get_screenshot_as_file(filename)
-                time.sleep(random() * MAX_SLEEP)
-            else:
-                debug("Already downloaded: %s" % filename)
+        for url in self.iter_urls():
+            self.save_image(url)
 
-        # Increment the month index.
-        month_index += 1
+    def main(self):
+        with self as client:
+            try:
+                client.download_images()
+            except Exception as exc:
+                self.exception(exc)
+
+
+def download_images():
+    Client().main()
 
 
 if __name__ == "__main__":
-    try:
-        with Headless() as hd:
-            download(hd)
-    except Exception as exc:
-        logger.exception(exc)
-        import pdb; pdb.set_trace()
+    download_images()
 
